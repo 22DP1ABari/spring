4fc852e77b862f028088c56b861e0ba696f15624
22DP1ABari <131352811+22DP1ABari@users.noreply.github.com>
2024-01-31 15:02:24 +0200

    Create Sorter.java

---
+++ src/test/java/lv/rvt/Sorter.java
@@ -0,0 +1,64 @@
+package lv.rvt;
+
+import java.util.Arrays;
+
+public class Sorter {
+    public static int smallest(int[] array) {
+        int smallest = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < smallest) {
+                smallest = array[i];
+            }
+        }
+        return smallest;
+    }
+
+    public static int indexOfSmallest(int[] array) {
+        int smallest = array[0];
+        int index = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < smallest) {
+                smallest = array[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public static int indexOfSmallestFrom(int[] array, int startIndex) {
+        int index = startIndex;
+        int smallest = array[index];
+
+        for (int i = startIndex; i < array.length; i++) {
+            if (array[i] < smallest) {
+                smallest = array[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+
+    public static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    public static void sort(int[] array) {
+        int index = 0;
+
+        while (index < array.length) {
+            swap(array, index, indexOfSmallestFrom(array, index));
+            System.out.println(Arrays.toString(array));
+
+            index += 1;
+        }
+
+    }
+
+
+
+   
+}
