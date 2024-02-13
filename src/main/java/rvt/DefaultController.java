a329aab09bb0d59e4d0de0dfd56c12ae82c0888f
22DP1ABari <131352811+22DP1ABari@users.noreply.github.com>
2024-02-13 12:20:32 +0200

    Update DefaultController.java

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -1,9 +1,12 @@
 package rvt;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,15 +18,25 @@ public class DefaultController {
     }
     @GetMapping(value = "/about")
     ModelAndView about(@RequestParam HashMap<String, String> queryStringParams) {
-        ModelAndView modelAndView = new ModelAndView(viewName:"about");
+        ModelAndView modelAndView = new ModelAndView("about");
 
-        String name = queryStringParams.get(key:"name");
+        String name = queryStringParams.get("name");
 
-        String id = queryStringParams.get(key:"id");
-        String title = queryStringParams.get(key:"title");
-        String description = queryStringParams.get(key:"descripiton");
+        String id = queryStringParams.get("id");
+        String title = queryStringParams.get("title");
+        String description = queryStringParams.get("descripiton");
+
+        return modelAndView;
 
         // CsvManager manager = new CsvManager(CsvManager)
+    }
+
+    @GetMapping(value="/test")
+    @ResponseBody()
+    public String testAction() {
+        String name = "John";
+
+        return name;
     }
 
 }
